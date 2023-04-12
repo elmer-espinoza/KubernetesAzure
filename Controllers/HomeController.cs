cd156b3781b941895f0ccc333121e6607987bedf
elmer-espinoza <elmer.espinoza@gmail.com>
2023-04-11 23:21:44 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using KubernetesAzure.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KubernetesAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly EmployeeContext _dbContext;
+        public HomeController(EmployeeContext dbcontext) { _dbContext = dbcontext; }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View(_dbContext.Employees.OrderBy(e => e.Nombre));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KubernetesAzure.Models
+{
+
+    [Table(name: "ApiRest")]
+    public class Employee
+        {
+            [Key]
+            public int id { get; set; }
+
+            public string Nombre { get; set; }
+
+            public string DNI { get; set; }
+
+            public short? Edad { get; set; }
+
+            public string Telefono { get; set; }
+
+            public string Correo { get; set; }
+
+            public decimal? Basico { get; set; }
+
+            public DateTime? Ingreso { get; set; }
+
+            public bool? Activo { get; set; }
+        }
+
+        public class EmployeeContext : DbContext
+        {
+            public EmployeeContext(DbContextOptions options) : base(options) { }
+            public DbSet<Employee> Employees { get; set; }
+        }
+
+}
