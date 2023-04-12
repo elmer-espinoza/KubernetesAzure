3128fc2a9f9fa09f4e8a98d74307a51286a186ff
elmer-espinoza <elmer.espinoza@gmail.com>
2023-04-11 23:26:13 -0500

    Ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace KubernetesAzure.Controllers
         public HomeController(EmployeeContext dbcontext) { _dbContext = dbcontext; }
 
         //private readonly ILogger<HomeController> _logger;
-
         //public HomeController(ILogger<HomeController> logger)
         //{
         //    _logger = logger;
